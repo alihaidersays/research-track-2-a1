db590add96027b926a15cfa459f6b9d5645fe788
Ali Haider <ali.haider9211@gmail.com>
2023-07-10 04:53:37 +0000

    Delete rt2a1a.sh

--- rt2a1a.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gnome-terminal --tab --title="roscore" -- bash -c "source ros.sh; roscore"
-gnome-terminal --tab --title="user_interface" -- bash -c "sleep 1; source ros.sh; roslaunch rt2_assignment1 sim.launch"
